9023f6d6806d7c04d4c4351e646352af8a5af9d6
ITSchool <itschool@mail.ru>
2023-05-20 12:59:23 +0600

    update

---
+++ cookie.php
@@ -0,0 +1,17 @@
+<?php
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Последнее обновление: секунд назад</p> 
+    <p>Последнее обновление: больше часа назад</p> 
+    <p><a href="cookie_update.php">Обновить</a></p>
+    <p><a href="cookie_clear.php">Очистить</a></p>
+</body>
+</html>
